f71b671951a9bc3174bac74daaf418e0a453f51c
josiedpb14 <110139188+josiedpb14@users.noreply.github.com>
2023-07-21 11:11:31 +0000

    NewSolution

--- src/models.py
+++ src/models.py
@@ -1,26 +1,50 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Text
-from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Text, Table, Enum
+from sqlalchemy.orm import relationship, declarative_base, backref
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
+user_conversations = Table('conversations',Base.metadata,
+                           Column('conv_id',Integer,primary_key=True),
+                           Column('user1_id',Integer,ForeignKey('users.id')),
+                           Column('user2_id',Integer,ForeignKey('users.id')),
+                           Column('conv',Text))
+
+# user_conversations = Table('conversations',Base.metadata,
+#                            Column('user1_id',Integer,ForeignKey('conversation.id'),primary_key=True),
+#                            Column('user2_id',Integer,ForeignKey('users.id'),primary_key=True),
+#                            Column('conv',Text))
+
+user_followers = Table('followers',Base.metadata,
+                      Column('user_from_id',Integer,ForeignKey('follower.id'),primary_key=True),
+                      Column('user_to_id',Integer,ForeignKey('users.id'),primary_key=True))
 
 class User(Base):
     __tablename__ = 'users'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(50))
-    email = Column(String)
-    password = Column(String)
     username = Column(String(250), nullable=False)
+    firstname = Column(String)
+    lastname = Column(String)
+    email = Column(String)
     posts = relationship('Post',backref='users',lazy=True)
     likes  =relationship('Like',backref='users',lazy=True)
     comments = relationship('Comment',backref='users',lazy=True)
-    friends = relationship('Friend',backref='users',lazy=True)
-    conversations = relationship('Conversation',backref='users',lazy=True)
+    followers = relationship('Follower',secondary = user_followers,lazy='subquery',
+                             backref=backref('users',lazy=True))
+    # conversations = relationship('user',secondary=user_conversations,lazy='subquery',
+    #                              backref = backref('users',lazy = True))
+    Conversations = relationship('User',secondary = user_conversations,
+                                 primaryjoin = id == user_conversations.c.user1_id,
+                                 secondaryjoin = id == user_conversations.c.user2_id,
+                                 backref = backref('users',lazy = True))
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer,primary_key=True)
 
 class Post(Base):
     __tablename__ = 'posts'
@@ -30,6 +54,14 @@ class Post(Base):
     footer = Column(String(600))
     likes = relationship('Like',backref='posts',lazy=True)
     comments = relationship('Comment',backref='comments',lazy=True)
+    post_media = relationship('Media',backref='post',lazy=True)
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer,primary_key = True)
+    type = Column(Enum)
+    url = Column(String)
+    post_id = Column(Integer,ForeignKey('posts.id'))
 
 class Comment(Base):
     __tablename__ = 'comments'
@@ -43,17 +75,9 @@ class Like(Base):
     user_id = Column(Integer,ForeignKey('users.id'),primary_key=True)
     post_id = Column(Integer,ForeignKey('posts.id'),primary_key=True)
 
-class Friendship(Base):
-    __tablename__ = 'friendships'
-    # id = Column(Integer,primary_key=True)
-    friend1_id = Column(Integer,ForeignKey('users.id'),primary_key=True)
-    friend2_id = Column(Integer,ForeignKey('users.id'),primary_key=True)
-
-class Conversation(Base):
-    __tablename__ = 'conversations'
-    user1_id = Column(Integer,ForeignKey('users.id'),primary_key=True)
-    user2_id = Column(Integer,ForeignKey('users.id'),primary_key=True)
-    conv = Column(Text)
+# class Conversation(Base):
+#     __tablename__ = 'conversation'
+#     id = Column(Integer,primary_key=True)
 
 ## Draw from SQLAlchemy base
 try:
